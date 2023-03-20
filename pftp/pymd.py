14e2362a07e39ebaeb94e7724bcaef267bc04251
ImAKappa <imaninconsp1cuouskappa@gmail.com>
2023-03-20 11:33:48 -0400

    Update py to md converter

--- pftp/pymd.py
+++ pftp/pymd.py
@@ -1,6 +1,26 @@
 """Converts Python source code examples to markdown
 
+Example: Writing mkdocs material documentation that displays English and Python sentences in a content tab
 
+```src.py
+def add_nums():
+    '''Write the sum of 1 and 2'''
+    print(f"{1 + 2 =}")
+```
+
+can be converted to
+
+```output.md
+### Add nums
+
+=== "English"
+
+    Write the sum of 1 and 2
+
+=== "Python"
+
+    print(f"{1 + 2 =}")
+```
 """
 
 # TODO: Manipulate ast to test that no example throws an error
@@ -8,54 +28,51 @@
 
 import ast
 from pathlib import Path
+from typing import Any
+from collections import namedtuple
 
+Fn = namedtuple("Fn", ["name", "docstring", "body"])
 
-class PyMDVisitor(ast.NodeVisitor):
-    """Converts AST to specified markdown format
+def fn_to_md(fn: Fn) -> str:
+    title: str = fn.name.replace("_", " ").title()
+    body: str = fn.body.replace('\n', '\n    ')
+    """Converts a FunctionDef to markdown"""
+    s = f"""### {title}
 
-    Example: Writing mkdocs material documentation that displays English and Python sentences in a content tab
+=== "English"
 
-    ```src.py
-    def add_nums():
-        '''Write the sum of 1 and 2'''
-        print(f"{1 + 2 =}")
+    {fn.docstring}
+
+=== "Python"
+
+    ```python
+    {body}
     ```
+"""
+    return s
 
-    converts to =>
-
-    ```output.md
-    ### Add nums
-
-    === "English"
-
-        Write the sum of 1 and 2
-
-    === "Python"
-
-        print(f"{1 + 2 =}")
-    ```
-
-    """
+class FnDefVisitor(ast.NodeVisitor):
+    """Extracts and formats data from FunctionDef's in Python source code"""
 
     def __init__(self, src: str):
         self.src = src
+        self.fns: list[Fn] = list()
 
-    def visit_FunctionDef(self, node: FunctionDef) -> Any:
-        return super().visit_FunctionDef(node)
+    def fn_body_str(self, node: ast.FunctionDef) -> str:
+        """Converts the FunctionDef body to a string of the source"""
+        return "\n".join([ast.get_source_segment(self.src, n) for n in node.body])
 
-    def generic_visit(self, node):
-        # print(f"entering {ast.dump(node)}")
-        if isinstance(node, ast.FunctionDef):
-            # print(ast.dump(node))
-            # print(ast.get_source_segment(self.src, node))
-            for n in node.body:
-                print(ast.get_source_segment(self.src, n))
-            print(ast.get_docstring(node))
-            print("-"*10)
-        super().generic_visit(node)
+    def visit_FunctionDef(self, node: ast.FunctionDef) -> Any:
+        fn = Fn(
+            node.name,
+            ast.get_docstring(node),
+            self.fn_body_str(node)
+        )
+        self.fns.append(fn)
+        return node
 
 
-p = Path(r"docs\1-Getting-Started\reading.py")
+p = Path("docs/1-Getting-Started/reading.py")
 
 with open(p, mode="r", encoding="utf-8") as f:
     content = f.read()
@@ -63,6 +80,9 @@ with open(p, mode="r", encoding="utf-8") as f:
 
 tree = ast.parse(content)
 
-visitor = PyMDVisitor(content)
+visitor = FnDefVisitor(content)
 
 visitor.visit(tree)
+
+for fn in visitor.fns:
+    print(fn_to_md(fn))
